ce3960fa1ab67683fde5d0345b49e00e8b6c93ab
Pedro <pedrovalentepaulino26@gmail.com>
2025-03-15 19:23:09 -0300

    Atualizando com outro construtor

--- POO/ClassePessoa/ClassePessoa/Program.cs
+++ POO/ClassePessoa/ClassePessoa/Program.cs
@@ -11,6 +11,13 @@ class Pessoa
         _endereco = endereco;
         _telefone = telefone;
     }
+
+    public Pessoa()
+    {
+        _nome = "Nome não descrito";
+        _endereco = "Endereço não descrito";
+        _telefone = "Telefone não descrito";
+    }
     public string primeiroNome()
     {
         return _nome;
@@ -22,6 +29,9 @@ class Program
     static void Main(string[] args)
     {
         Pessoa pessoa = new Pessoa("Pedro", "Rua Jacutinga", "31997944389");
+        Pessoa pessoa1 = new Pessoa();
         Console.WriteLine(pessoa.primeiroNome());
+        Console.WriteLine(pessoa1.primeiroNome());
+
     }
 }
--- POO/ClassePessoa/ClassePessoa/obj/Debug/net8.0/ClassePessoa.AssemblyInfo.cs
+++ POO/ClassePessoa/ClassePessoa/obj/Debug/net8.0/ClassePessoa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassePessoa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+598dc6f623565812461db77a8592d7325ee1d20e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassePessoa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassePessoa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
