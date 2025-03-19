dca6f1a8e2988d32f6d03efa11ea728296647bf7
Pedro <pedrovalentepaulino26@gmail.com>
2025-03-19 11:26:58 -0300

    Entendendo classe parcial

---
+++ POO/PartialClass/PartialClass/PartialClass1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass
+{
+    public partial class PartialClass1
+    {
+        public string tarefa {  get; set; }
+    }
+}
---
+++ POO/PartialClass/PartialClass/Program.cs
@@ -0,0 +1,13 @@
+﻿using PartialClass;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        PartialClass1 estudar = new PartialClass1();
+        estudar.tarefa = "estudar";
+        estudar.Tarefa();
+    }
+}
