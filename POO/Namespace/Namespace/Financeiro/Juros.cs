38e87a3281a095d5eccf367a9ee546cb35b2728c
Pedro <pedrovalentepaulino26@gmail.com>
2025-03-19 10:31:01 -0300

    Praticando namespaces

---
+++ POO/Namespace/Namespace/Financeiro/Juros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financeiro
+{
+    public class Juros
+    {
+        public Juros()
+        {
+            Valor = 5;
+        }
+        public int Valor { get; set; }
+    }
+}
---
+++ POO/Namespace/Namespace/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Contabil;
+using Financeiro;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Financeiro.Juros financeiro = new Financeiro.Juros();
+        Contabil.Juros contabil = new Contabil.Juros();
+
+        Console.WriteLine("Classe no namespace financeiro: {0}", financeiro.Valor);
+        Console.WriteLine("Classe no namespace contabil: {0}", contabil.Valor);
+    }
+}
