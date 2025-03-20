4f1ca323a590bab7a7fca50663386a25fe682668
Pedro <pedrovalentepaulino26@gmail.com>
2025-03-20 12:47:40 -0300

    Praticando sobreposição

---
+++ POO/Polimorfismo/Polimorfismo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Pai
+{
+    public string i = "Pai";
+    public virtual void Papai()
+    {
+        Console.WriteLine(i);
+    }
+}
+
+class Filho : Pai
+{
+    public new string i = "Filho";
+    public override void Papai()
+    {
+        Console.WriteLine("----------");
+        base.Papai();
+        Console.WriteLine(i);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pai pai = new Pai();
+        Filho filho = new Filho();
+        pai.Papai();
+        filho.Papai();
+    }
+}
