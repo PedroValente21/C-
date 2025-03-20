3c3b7ef768910ac859899873931f7b975d366aac
Pedro <pedrovalentepaulino26@gmail.com>
2025-03-20 10:46:15 -0300

    Praticando herança

--- POO/ClassePessoa/ClassePessoa/Program.cs
+++ POO/ClassePessoa/ClassePessoa/Program.cs
@@ -24,14 +24,51 @@ class Pessoa
     }
 }
 
+class Fisica : Pessoa
+{
+    private string cpf;
+
+    public Fisica()
+    {
+        this.cpf = "000.000.000.00";
+    }
+
+    public string CPF
+    {
+        get { return this.cpf; }
+        set { this.cpf = value; }
+    }
+}
+
+class Juridica : Pessoa
+{
+    private string cnpj;
+    public Juridica()
+    {
+        this.cnpj = "00.000.000/0000-00";
+    }
+    public string CNPJ
+    {
+        get { return this.cnpj; }
+        set { this.cnpj = value; }
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
         Pessoa pessoa = new Pessoa("Pedro", "Rua Jacutinga", "31997944389");
         Pessoa pessoa1 = new Pessoa();
+        Fisica fisica = new Fisica();
+        Juridica juridica = new Juridica();
         Console.WriteLine(pessoa.primeiroNome());
         Console.WriteLine(pessoa1.primeiroNome());
-
+        Console.WriteLine("{0} - {1}",fisica.primeiroNome(), fisica.CPF);
+        Console.WriteLine("{0} - {1}",juridica.primeiroNome(), juridica.CNPJ);
+        Console.WriteLine();
+        Console.WriteLine("Digite o CPF da pessoa: ");
+        fisica.CPF = Console.ReadLine();
+        Console.WriteLine("{0} - {1}", fisica.primeiroNome(), fisica.CPF);
     }
 }
--- POO/ClassePessoa/ClassePessoa/obj/Debug/net8.0/ClassePessoa.AssemblyInfo.cs
+++ POO/ClassePessoa/ClassePessoa/obj/Debug/net8.0/ClassePessoa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassePessoa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+598dc6f623565812461db77a8592d7325ee1d20e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbfced0927dcbfd27a3628a4fad61245b33ed89")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassePessoa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassePessoa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
