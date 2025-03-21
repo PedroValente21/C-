8e9386aaff77ef94d981650a6b8d97f531bc2529
Pedro <pedrovalentepaulino26@gmail.com>
2025-03-21 12:33:36 -0300

    Testando event usando delegate

---
+++ POO/Event&Delegate/Event&Delegate/Program.cs
@@ -0,0 +1,43 @@
+﻿class Alarme
+{
+    public delegate void Acordar(string mensagem);
+    public event Acordar TempoEsgotado;
+
+    public void DisparaEvento()
+    {
+        AoEsgotarTempo();
+    }
+
+    public void AoEsgotarTempo()
+    {
+        if (TempoEsgotado != null)
+        {
+            TempoEsgotado("Tempo esgotado!");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alarme alarme = new Alarme();
+
+        alarme.TempoEsgotado += MensagemConsole;
+        alarme.DisparaEvento();
+        alarme.TempoEsgotado += MensagemConsole2;
+        alarme.DisparaEvento();
+        alarme.TempoEsgotado -= MensagemConsole;
+        alarme.DisparaEvento();
+    }
+
+    public static void MensagemConsole(string mensagem)
+    {
+        Console.WriteLine(mensagem);
+    }
+
+    public static void MensagemConsole2(string mensagem)
+    {
+        Console.WriteLine("Mensagem2");
+    }
+}
